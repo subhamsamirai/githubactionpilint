6b985e118ee7b59fde4a8d9fd21dc535e6b60480
Subham samir <ssrp2814@gmail.com>
2026-09-16 15:37:09 +0530

    fixed

--- app.py
+++ app.py
@@ -5,14 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
- return render_template('index.html')
+    return render_template('index.html')
 
 
 @app.route('/health')
 def health():
- return 'Server is up and running'
-
-
-# if __name__ == '__main__':
-# 	app.run(debug=True)
+    return 'Server is up and running'
 
